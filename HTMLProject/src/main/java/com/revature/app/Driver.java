13944a88ad6ca5e79f8ce8fcf41c28d7f36c3430
Jacob Davis <jacob.marquez.davis@hotmail.com>
2022-01-21 10:17:28 -0500

    HTML Project

---
+++ HTMLProject/src/main/java/com/revature/app/Driver.java
@@ -0,0 +1,38 @@
+package com.revature.app;
+
+import com.revature.user.User;
+import com.revature.user.UserDao;
+
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+import io.javalin.http.staticfiles.Location;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Javalin app = Javalin.create(
+				config -> {
+					config.addStaticFiles("/html", Location.CLASSPATH);
+				}).start(7070);
+		
+		app.post("/users/form", postUser);
+	}
+
+	public static Handler postUser = ctx -> {
+		String username = ctx.formParam("username"),
+				password = ctx.formParam("password"),
+				firstName = ctx.formParam("firstName"),
+				lastName = ctx.formParam("lastName"),
+				birthMonth = ctx.formParam("birthMonth");
+		
+		//System.out.println(firstName + " " + lastName + " has just signed up!");
+		User u = new User(username, password, firstName, lastName, birthMonth);
+		
+		UserDao dao = new UserDao(ConnectionManager.getConnection());
+		dao.createUser(u);
+		
+		//ctx.redirect("localhost:7070/html/index.html");
+	};
+}
